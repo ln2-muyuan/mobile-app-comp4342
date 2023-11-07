f1e6de96ded355cc29fa291943b79e2a7165f340
Muyuan <lmy2019@outlook.com>
2023-11-07 14:28:10 +0800

    Revert "added navbar"
    This reverts commit 7af8e61ace7761620dbdedb7e200b016ab02501c.

--- Frontend/src/screens/Profile.js
+++ Frontend/src/screens/Profile.js
@@ -1,20 +1,16 @@
 
 import React from 'react';
 import { View, Text } from 'react-native';
-import Navbar from '../components/Navbar';
 
 
 const Profile = ({ navigation, route }) => {
   const { name } = route.params;
 
   return (
-
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 24 }}>Profile Screen</Text>
       <Text style={{ fontSize: 18, marginTop: 10 }}>Name: {name}</Text>
-      <Navbar />
     </View>
- 
   );
 };
 
