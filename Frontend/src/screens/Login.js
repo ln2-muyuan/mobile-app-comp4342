85907529651417d0d930835ddf6ed111b535ba29
gcctbilly <gcctxhl@163.com>
2023-11-07 18:17:41 +0800

    Add login page

--- Frontend/src/screens/Login.js
+++ Frontend/src/screens/Login.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Login = () => {
     return (
-      <View style={styles.container}>
-        <Text style={styles.red}>Login</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={{paddingHorizontal:25}}>
+            <Text style={styles.loginText}>Login</Text>
+            <View style={{flexDirection:"row", borderBottomColor:"#ccc", borderBottomWidth:1}}>
+                <TextInput placeholder='Username'/>
+            </View>
+            <View style={{flexDirection:"row", borderBottomColor:"#ccc", borderBottomWidth:1}}>
+                <TextInput placeholder='Password' secureTextEntry={true}/>
+            </View>
+        </View>
+      </SafeAreaView>
     );
   };
   
   const styles = StyleSheet.create({
     container: {
-      marginTop: 50,
+      flex:1,
+      justifyContent:"center",
     },
-    red: {
-      color: 'red',
-    },
+    loginText:{
+        fontSize:28,
+        fontWeight:"500",
+        color:"#333",
+        marginBottom:30
+    }
   });
   
   export default Login;
