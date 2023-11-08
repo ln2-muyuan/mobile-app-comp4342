085cd26fb293a0ff38c27bc929bc00807b360e90
gcctbilly <gcctxhl@163.com>
2023-11-08 14:44:51 +0800

    userApi structure

---
+++ Frontend/src/api/userApi.js
@@ -0,0 +1,17 @@
+import { post } from "../utils/request"
+
+export const register = () => {
+
+}
+
+export const login = (username, password) => {
+}
+
+export const logout = () => {
+
+}
+
+//get user information {username, nickname}
+export const getUserInfo = () => {
+
+}
