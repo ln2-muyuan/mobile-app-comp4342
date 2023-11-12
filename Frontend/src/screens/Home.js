5c43cdb90c901fe82a0d89ee2286ab90a0784cb0
Muyuan <lmy2019@outlook.com>
2023-11-12 17:43:44 +0800

    slight change

--- Frontend/src/screens/Home.js
+++ Frontend/src/screens/Home.js
@@ -26,7 +26,7 @@ const Home = ({navigation}) => {
   return (
     <SafeAreaView>
       <View>
-        <ScrollView style={{flexGrow: 0.86}}>
+        <ScrollView style={{flexGrow: 0.91}}>
           <Header/>
           <Button title="Go to Login Page" onPress={() => navigation.navigate('Login')} />
           <PostSection userName="胡桃桃桃砸" userAvatar={require("../assets/profile.png")} postTime="11-11" title="玩原这么久最后悔的事是什么" contentText="好吧，我是把阿莫斯精炼了，当时我应该是五十多级，然后常驻保底出了两把，我当时也不养弓箭手，然后我就跟我同期玩的同学吐槽（当时还算是个啥都不懂的萌新呢，只有两个五星角色，一个莫娜一个万叶）她就给我来了一句精炼呗，我想着反正也不用弓，然后就精炼了，现在看到就有点心梗" imageURL={multipleImages}/>
