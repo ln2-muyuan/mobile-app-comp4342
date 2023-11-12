31b48e545da50c72231fee957789c21ed68857e1
Muyuan <lmy2019@outlook.com>
2023-11-12 17:48:59 +0800

    updated

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Profile from './src/screens/Profile';
-import { NavigationContainer } from '@react-navigation/native';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
+
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
@@ -13,13 +14,10 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Profile" options={{headerShown:false}} component={Profile} />
         <Stack.Screen name="Login" options={{headerShown:false}} component={Login} />
         <Stack.Screen name="Register" options={{headerShown:false}} component={Register} />
       </Stack.Navigator>
-
-
-
     </NavigationContainer>
   );
 }
