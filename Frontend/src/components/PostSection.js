fd99ed39241e52bb1380508876fd008ad93170b2
melody777k <94382552+melody777k@users.noreply.github.com>
2023-11-15 13:21:51 +0800

    show video

--- Frontend/src/components/PostSection.js
+++ Frontend/src/components/PostSection.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import ImageGallery from './ImageGallery';
+import Video from 'react-native-video';
+import Slider from '@react-native-community/slider';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const PostSection = ({ userAvatar, userName, postTime, title, contentText, imageURL }) => {
+const PostSection = ({ userAvatar, userName, postTime, title, contentText, imageURL, videoURL }) => {
   const hasImage = Boolean(imageURL);
   const hasText = Boolean(contentText);
+  const hasVideo = Boolean(videoURL);
+  const [paused, setPaused] = React.useState(true);
+  const [progress, setProgress] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const videoPlayer = useRef(null);
 
   return (
     <View style={styles.container}>
@@ -26,6 +34,41 @@ const PostSection = ({ userAvatar, userName, postTime, title, contentText, image
         <Text style={styles.blank}></Text>
       )}
 
+      {hasVideo && (
+        <>
+          <TouchableWithoutFeedback onPress={() => setPaused(!paused)}>
+            <View style={styles.videoWrapper}>
+              <Video 
+                ref={videoPlayer}
+                source={videoURL}
+                style={styles.video} 
+                resizeMode="cover" 
+                repeat={true} 
+                paused={paused}
+                onProgress={({ currentTime, playableDuration }) => {
+                  setProgress(currentTime);
+                  setDuration(playableDuration);
+                }}
+               />
+              {paused && (
+                <View style={styles.overlay}>
+                  <Icon name="play-circle-outline" size={50} color="#FFF" />
+                </View>
+              )}
+            </View>
+          </TouchableWithoutFeedback>
+          
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={1}
+            value={progress / duration}
+            onSlidingComplete={value => {
+              videoPlayer.current.seek(value * duration);
+            }}
+          />
+        </>
+      )}
 
       {hasImage && (
         <ImageGallery images={imageURL} />
@@ -78,6 +121,30 @@ const styles = StyleSheet.create({
   blank: {
     fontSize: 5,
   },
+  slider: {
+    width: '100%', 
+    height: 40
+  },
+  video: {
+    width: '100%', 
+    height: 300, 
+  },
+  videoWrapper: {
+    position: 'relative',
+  },
+  overlay: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{translateX: -40}, {translateY: -40}], 
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    borderRadius: 50,
+    width: 80,
+    height: 80,
+  },
+
 });
 
 export default PostSection;
